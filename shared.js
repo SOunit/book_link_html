2c39b3525b116c561f01256b1d28b473cf0c4212
SOunit <workbookwork7@gmail.com>
2021-07-24 03:42:28 -0700

    add side-menu

--- shared.js
+++ shared.js
@@ -2,12 +2,15 @@ console.log('hello from js');
 
 const sidebarButton = document.querySelector('.sidebar-button');
 const backdrop = document.querySelector('.backdrop');
+const mobileNav = document.querySelector('.mobile-nav');
 
 sidebarButton.addEventListener('click', () => {
   console.log('click');
   backdrop.classList.add('open');
+  mobileNav.classList.add('open');
 });
 
 backdrop.addEventListener('click', () => {
   backdrop.classList.remove('open');
+  mobileNav.classList.remove('open');
 });
